b8c07cfab23a0e51091e25711841c90b64c5cf02
Christian Marques <christianmarquesos@poli.ufrj.br>
2021-05-05 13:05:27 -0300

    Criação do arquivo
    Comentários iniciais sobre a explicação do funcionamento

---
+++ main.py
@@ -0,0 +1,26 @@
+#################################################################################
+# Universidade Federal do Rio de Janeiro
+# Disciplina: Inteligência Artificial - EEL891
+# Professor: Heraldo L. S. Almeida
+# Desenvolvedor: Chritian Marques de Oliveira Silva
+# DRE: 117.214.742
+# Trabalho: Desafio das 8 rainhas utilizando Algoritimo Genético
+#################################################################################
+
+# PANORAMA GERAL
+# 1) Serão 6 indivíduos a cada geração - cada indivídio será um tabuleiro
+# 2) Um tabuleiro 8x8 com 8 rainhas
+# 3) Cada rainha ataca em qualquer ponto no movimento em "+" e em "x" até o limite do tabuleiro 
+# 4) Objetivo - Posicionar todas elas no tabuleiro de modo que nenhuma possa atacar outra 
+# 5) Forma de quantificar o melhor:
+#   5.1) Depois do posicionamento das rainhas, cada ataque (tanto de A para B qaunto de B para A) será contado
+#   5.2) Os melhores indivíduos serão os com as menores pontuações (o objetivo é no final ter 0 ataques)
+#   5.3) O mínimo de ataques é 0 (melhor disposição - nenhum se ataca) e o máximo é 56 (pior disposição - todos se atacam)
+
+# REGRAS
+# 1) Crossover:
+#   1.1)
+# 2) Mutação:
+#   2.1)
+# 3) Seleção:
+#   3.1) Elitismo - 
